f0e30c1564134740a63890e68a253441a8208741
VaibhavMagar1504 <vaibhavmagar2222@gmail.com>
2026-01-24 00:33:42 +0530

    Initial frontend upload

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL= "http://localhost:8071/user";
+const BASE_URL = import.meta.env.VITE_API_URL + "/user";
 
  export const registerUser = async (userData) => {
   return await axios.post(`${BASE_URL}/userReg`, userData);
